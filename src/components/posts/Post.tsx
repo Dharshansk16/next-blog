817dcd859b0adbbbc45cc52d00d817ee5a859587
Dharshansk16 <dcodecraft@gmail.com>
2024-07-24 11:37:01 +0530

    feat: prisma setup

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import UpVote from "../buttons/UpVote";
 
 export default async function Post({ id }: { id: String }) {
   const response = await fetch(`https://dummyjson.com/posts/${id}`);
@@ -8,7 +7,6 @@ export default async function Post({ id }: { id: String }) {
     <div>
       <h1 className="text-4xl mb-4 font-bold">{post.title}</h1>
       <p className="text-lg leading-normal">{post.body}</p>
-      <UpVote />
     </div>
   );
 }
