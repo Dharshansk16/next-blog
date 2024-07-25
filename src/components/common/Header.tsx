b1d97d7faf569a10d3959530c164c027d6beff3f
Dharshansk16 <dcodecraft@gmail.com>
2024-07-25 13:39:06 +0530

    style: navbar items on hover

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -23,6 +23,7 @@ const navLinks = [
 export default function Header() {
   const { isAuthenticated } = useKindeBrowserClient();
   const pathName = usePathname();
+  const [isHoverIndex, setIsHoverIndex] = useState<number | null>(null);
   return (
     <div className="py-3  text-zinc-600 flex justify-between items-center px-4 font-semibold shadow-md">
       <h1 className="text-2xl px-4 text-zinc-100 items-center">
@@ -32,7 +33,17 @@ export default function Header() {
         <ul className="flex justify-between gap-4 px-4 items-center">
           {navLinks.map((item, index) => (
             <li
-              className={pathName === item.href ? "text-zinc-100" : ""}
+              onMouseEnter={() => {
+                setIsHoverIndex(index);
+              }}
+              onMouseLeave={() => {
+                setIsHoverIndex(null);
+              }}
+              className={
+                pathName === item.href || isHoverIndex === index
+                  ? "text-zinc-100"
+                  : ""
+              }
               key={index}
             >
               <Link href={item.href}>{item.label}</Link>
